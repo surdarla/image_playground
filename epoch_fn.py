f670a39072bb470d1914aceb478278ff892dcb9a
준수 김 <rlskejdk@naver.com>
2022-04-28 17:19:40 +0900

    Add accum_iter

--- epoch_fn.py
+++ epoch_fn.py
@@ -21,28 +21,31 @@ def train_one_epoch(epoch,model, train_loader,criterion, optimizer,device,scaler
         out = model(images)
       loss = criterion(out, targets) 
       acc1 = accuracy2(out,targets)
+      
+      if CFG.accum_iter > 1:
+            loss = loss / CFG.accum_iter
       losses.update(loss.item(),batch_size)
       top1.update(acc1.item(),batch_size)
       scaler.scale(loss).backward()
 
       if CFG.max_grad_norm: 
         grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(),CFG.max_grad_norm) 
-       
-      scaler.step(optimizer)
-      scaler.update()
-      optimizer.zero_grad()
+      
+      if (step+1) % CFG.accum_iter == 0:
+          scaler.step(optimizer)
+          scaler.update()
+          optimizer.zero_grad()
 
       scheduler.step()
 
-      end = time.time()
-      if (step+1) % CFG.print_freq == 0 or step == (len(train_loader)-1):
+      if step % CFG.print_freq == 0 or step == (len(train_loader)):
         print('Epoch: [{0}/{1}][{2}/{3}] '
               'Elapsed {remain:s} '
               'Loss: {loss.val:.4f}({loss.avg:.4f}) '
               'Grad: {grad_norm:.4f}  '
               'LR: {lr:.6f}  '
               'top1_avg: {top1.avg:.4f}  '
-              .format(epoch+1,CFG.epochs, step, len(train_loader), 
+              .format(epoch+1, CFG.epochs, step, len(train_loader), 
                       remain=timeSince(start, float(step+1)/len(train_loader)),
                       loss=losses,
                       grad_norm=grad_norm,
@@ -58,20 +61,21 @@ def valid_one_epoch(epoch,model, valid_loader, criterion,device,scheduler=None):
 
   losses = AverageMeter()
   top1 = AverageMeter()
-  top5 = AverageMeter()
   
   start = end = time.time()
   for step, (images, targets) in enumerate(valid_loader):
     images = images.to(device)
     targets = targets.to(device)
     batch_size = targets.size(0)
+    
     out = model(images)
+    
     loss = criterion(out, targets)
     acc1 = accuracy2(out,targets)
+    
     losses.update(loss, batch_size)
     top1.update(acc1,batch_size)
     
-    end = time.time()
     if step % CFG.print_freq == 0 or step == (len(valid_loader)-1):
         print('EVAL: [{0}/{1}] '
               'Elapsed {remain:s} '
@@ -85,15 +89,3 @@ def valid_one_epoch(epoch,model, valid_loader, criterion,device,scheduler=None):
 
   return losses.avg, top1.avg
 
-def inference_one_epoch(model, data_loader, device):
-  model.eval()
-  image_preds_all = []
-
-  for step, (images, _) in tqdm(enumerate(data_loader),total=len(data_loader)):
-      images = images.to(device).float()
-      
-      image_preds = model(images)  
-      image_preds_all += [torch.softmax(image_preds, 1).detach().cpu().numpy()]
-      
-  image_preds_all = np.concatenate(image_preds_all, axis=0)
-  return image_preds_all
