2f28880b66747d0d83befca916abef8fb269cc41
surdarla <rlskejdk123@gmail.com>
2022-06-28 17:31:14 +0900

    deepspeed implement

--- pl_train.py
+++ pl_train.py
@@ -5,13 +5,15 @@ from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.plugins import DeepSpeedPlugin
+from pytorch_lightning.strategies import DeepSpeedStrategy
 import wandb
 
 from data.cifar10.plcifar10 import CIFAR10Data
 from config import CFG
 from model.alexnet import AlexNetLit
 from model.vgg import VGG
+
+WANDB_NAME = "vgg19_deepspeed_batchscale"
 
 wandb.login(key=CFG.wandb_key)
 cifar = CIFAR10Data(CFG.data_dir, CFG.batch_size, 0)
@@ -21,16 +23,17 @@ cifar.setup(stage="fit")
 if CFG.MODEL is "alexnet":
     model = AlexNetLit(10, lr=CFG.min_lr)
 elif CFG.MODEL is "vgg":
-    model = VGG("VGG13")
-wandb_logger = WandbLogger(name="test1", project="image_playground")
+    model = VGG("VGG19")
+wandb_logger = WandbLogger(name=f"{WANDB_NAME}", project="image_playground")
 pl.seed_everything(43)
 trainer = pl.Trainer(
     # max_epochs=100, # max_epochs=-1
     accelerator="auto",
     # auto_lr_find=True,
+    auto_scale_batch_size="binsearch",
     accumulate_grad_batches={0: 8, 4: 4, 8: 1},
     gradient_clip_val=0.5,
-    gradient_clip_algorithm="value",
+    # gradient_clip_algorithm="value",
     detect_anomaly=True,
     devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
     logger=wandb_logger,
@@ -39,7 +42,7 @@ trainer = pl.Trainer(
         LearningRateMonitor(logging_interval="step"),
         TQDMProgressBar(refresh_rate=50),
     ],
-    plugins=DeepSpeedPlugin(
+    strategy=DeepSpeedStrategy(
         stage=3,
         offload_optimizer=True,
         offload_parameters=True,
@@ -66,6 +69,7 @@ trainer = pl.Trainer(
 # print(f"Suggested learning rate: {new_lr}")
 # model.hparams.lr = new_lr
 
+trainer.tune(model, datamodule=cifar)
 trainer.fit(model, cifar)
 trainer.test(model, cifar)
 trainer.save_checkpoint(CFG.pth_dir, f"{CFG.MODEL}.pth")
