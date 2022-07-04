2e0f8a464180dd4c26499d64ed6f8a21e03b3678
surdarla <rlskejdk123@gmail.com>
2022-07-04 14:09:53 +0900

    only optimizer no scheduler for lr_finder

--- model/lightningmodule.py
+++ model/lightningmodule.py
@@ -37,9 +37,10 @@ class MyModule(pl.LightningModule):
         # optimizer = DeepSpeedCPUAdam(self.parameters(), lr=self.learning_rate)
         # optimizer = FusedAdam(self.parameters(), lr=self.learning_rate)
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
-        lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            optimizer, max_lr=self.learning_rate, verbose=True, total_steps=1000
-        )
+        return optimizer
+        # lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        #     optimizer, max_lr=self.learning_rate, verbose=True, total_steps=1000
+        # )
         # lr_scheduler = ReduceLROnPlateau(
         #     optimizer, factor=0.1, patience=self.args.patience
         # )
@@ -53,11 +54,11 @@ class MyModule(pl.LightningModule):
         #     warmup_steps=1,
         #     gamma=0.75,
         # )
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": lr_scheduler,
-            "monitor": "VALID LOSS",
-        }
+        # return {
+        #     "optimizer": optimizer,
+        #     "lr_scheduler": lr_scheduler,
+        #     "monitor": "VALID LOSS",
+        # }
 
     def training_step(self, batch, batch_idx):
         images, targets = batch
